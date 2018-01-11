b2220615aae7577ab3fde5d0a075f8fa65b09377
kovacsgabo <kovacsgaboo@gmail.com>
2018-01-11 09:17:14 +0100

    initial commit

---
+++ gittie.java
@@ -0,0 +1,8 @@
+public class gittie{
+    public static void main(String[] args){
+        hello();
+    }
+    public static void hello(){
+        System.out.println("Hello i am gittie!");
+    }
+}
