6eabff3bafd6b4fdb2e16c8edabfde550f53b527
kovacsgabo <kovacsgaboo@gmail.com>
2018-01-11 09:22:49 +0100

    adding joke

--- gittie.java
+++ gittie.java
@@ -5,4 +5,9 @@ public class gittie{
     public static void hello(){
         System.out.println("Hello i am gittie!");
     }
+
+    public static void joke(){
+        System.out.println("Patient: Oh doctor, I’m just so nervous. This is my first operation. /n-
+        Doctor: Don't worry. Mine too.")
+    }
 }
